83ca8526ccab7a7c32f4e7fc3b3ec509a0a4350b
Pinto Tetteh Aaron <aaronpinto111@gmail.com>
2023-11-22 12:35:32 +0000

    establish database connection

---
+++ utils/auth.py
@@ -0,0 +1,41 @@
+import requests
+import json
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+KEYCLOAK_URL = os.getenv("KEYCLOAK_SERVER_URL")
+KEYCLOAK_REALM = os.getenv("KEYCLOAK_REALM_NAME")
+KEYCLOAK_CLIENT_ID = os.getenv("KEYCLOAK_CLIENT_ID")
+KEYCLOAK_CLIENT_SECRET = os.getenv("KEYCLOAK_CLIENT_SECRET")
+KEYCLOAK_ADMIN = os.getenv("KEYCLOAK_ADMIN_USERNAME")
+KEYCLOAK_ADMIN_PASSWORD = os.getenv("KEYCLOAK_ADMIN_PASSWORD")
+
+
+
+
+def get_user_token(username, password):
+    data = {
+        'grant_type': 'password',
+        'client_id': KEYCLOAK_CLIENT_ID,
+        'client_secret': KEYCLOAK_CLIENT_SECRET,
+        'username': username,
+        'password': password,
+    }
+    try:
+        response = requests.post(f"{KEYCLOAK_URL}/realms/{KEYCLOAK_REALM}/protocol/openid-connect/token", data=data)
+        return response.json()['access_token']
+    except Exception as e:
+        return {"message": str(e)}        
+
+def create_user(user):
+    headers = {
+        'Authorization': f'Bearer {get_user_token(KEYCLOAK_ADMIN, KEYCLOAK_ADMIN_PASSWORD)}',
+        'Content-Type': 'application/json',
+    }
+    try:
+        response = requests.post(f"{KEYCLOAK_URL}/admin/realms/{KEYCLOAK_REALM}/users", headers=headers, json=user)
+    except Exception as e:
+        return {"message": str(e)}
+    return response.status_code
